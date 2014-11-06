d83a044e24ec55970c98c9427b21ba323f985dd9
fsi206914 <liang.xjtu@gmail.com>
2014-11-06 10:49:30 -0600

    infer backup

--- src/main/scala/infer/infer.scala
+++ src/main/scala/infer/infer.scala
@@ -11,31 +11,39 @@ import probabilitymonad.Distribution._
 
 object Infer{
 
+  abstract class Clause(src1: Int, src2: Int){
+  	// result is to differentiate the clause is true or not.
+  	val result: Boolean = false;
+  }
+
+  case class FrdClause(val numMutualFrds: Int, src1: Int, src2: Int) extends Clause(src1, src2)
+  case class CommClause(val numMutualComms: Int, src1: Int, src2: Int) extends Clause(src1, src2)
+
+
   val rand = new Random(System.currentTimeMillis())
   var frdsMapGlobal = Map[Int, ArrayBuffer[Int]]()
-  var frdsRelation= Map[(Int, Int), Boolean]()
-  var frdsRelationEvidence = Map[(Int, Int), Boolean]()
-  var formulaMap = Map[(Int, Int, Int), Boolean]()
+  var FRAlwaysTrue= Map[(Int, Int), Boolean]()
+  var FRDynamic = Map[(Int, Int), Boolean]()
+  var formulaMap = Map[Clause, Boolean]()
 
   def ifTwoPersonKnow(src: Int, dest: Int) = frdsMapGlobal(src).contains(dest)
 
   def genRandomBoolean() = if(rand.nextDouble()<0.5) true else false
 
-  def assignBin(aFrdsRelation: Map[(Int, Int), Boolean]) = {
-    for( (i, j)<- frdsRelation ){
-      if(frdsRelationEvidence.contains(i))
-        aFrdsRelation += i ->true
+  def assignBin(aFRAlwaysTrue: Map[(Int, Int), Boolean]) = {
+    for( (i, j)<- FRAlwaysTrue ){
+      if(FRDynamic.contains(i))
+        aFRAlwaysTrue += i ->true
       else
-        aFrdsRelation += i -> genRandomBoolean()
+        aFRAlwaysTrue += i -> genRandomBoolean()
     }
   }
 
   def ifFormulaTrue(src1: Int, src2: Int, src3: Int) = 
-    if(ifTwoPersonKnow(src1, src2) && ifTwoPersonKnow(src2, src3) && !ifTwoPersonKnow(src1, src3) )
       false
-    else true
 
-  def apply(frdsMap: Map[Int, ArrayBuffer[Int]], inferSet: scala.collection.Set[Int])(src1: Int, src2: Int) = {
+
+  def apply(frdsMap: Map[Int, ArrayBuffer[Int]], commMap: Map[Int, ArrayBuffer[Int]])(src1: Int, src2: Int) = {
   
     frdsMapGlobal = frdsMap
     val conf = ConfigFactory.load
@@ -44,41 +52,34 @@ object Infer{
      * In the whole inferSet, if two people know each other, the two is signed
      * to true. If not, a random boolean is assigned to the relationship.
      */
-    for(i<- inferSet; j<- inferSet; if(i<j && !frdsRelation.contains( (i,j) ))){
-      if(ifTwoPersonKnow(i,j)){
-        frdsRelation += (i,j)->true
-        frdsRelationEvidence  += (i,j)->true
+    for( (i1, i2)<- frdsMap; (j1, j2)<- frdsMap; if(i1<j1) ){
+      if(ifTwoPersonKnow(i1,j1)){
+        FRAlwaysTrue += (i1,j1)->true
+        FRDynamic += (i1,j1)->true
       }
       else
-        frdsRelation += (i,j)->genRandomBoolean
+        FRDynamic += (i1,j1)->genRandomBoolean
     }
 
     /*
-     * frdsRelation should not contain (k,v) (v<=k)
+     * because FRAlwaysTrue should not contain (k,v) (v<=k), we can ignore half matrix.
      */
-    for((i1, i2)<- frdsRelation; (j1, j2)<-frdsRelation.filter{ case (k,v) =>k._1==i1._2}; if(!( i2== true && j2==true && ifTwoPersonKnow(i1._1, j1._2)==false )) ){
-      if(rand.nextDouble()<0.3){
-        if(ifTwoPersonKnow(i1._1, i1._2) && ifTwoPersonKnow(i1._2, j1._1) )
-          println(ifTwoPersonKnow(i1._1, i1._2) + " " + ifTwoPersonKnow(i1._2, j1._1));
-        formulaMap += (i1._1, i1._2, j1._2) -> ifFormulaTrue(i1._1, i1._2, j1._2);
-      }
-    }
     println("formulaMap size = " + formulaMap.size)
-    walkSAT(formulaMap)
+    // walkSAT(formulaMap)
   }
 
 
   def walkSAT(formulas: Map[(Int, Int, Int), Boolean]) = {
-    val walkFrdsRelation = Map[(Int, Int), Boolean]();
-    assignBin(walkFrdsRelation)
-    for((i1, i2)<- walkFrdsRelation; (j1, j2)<-walkFrdsRelation.filter{ case (k,v) =>k._1==i1._2 })
-      formulas((i1._1, i1._2, j1._2)) = ifFormulaTrue(i1._1, i1._2, j1._2)
-    var falseFormulas = formulas.filter{ case(k,v)=> v==false}.toList;
-    println("falseFormulas size = "+ falseFormulas.length)
-    var count = 1;
-    while(count< 10000){
-      val formula = falseFormulas(rand.nextInt(falseFormulas.size)) 
-      count += 1
-    }
+    // val walkFRAlwaysTrue = Map[(Int, Int), Boolean]();
+    // assignBin(walkFRAlwaysTrue)
+    // for((i1, i2)<- walkFRAlwaysTrue; (j1, j2)<-walkFRAlwaysTrue.filter{ case (k,v) =>k._1==i1._2 })
+    //   formulas((i1._1, i1._2, j1._2)) = ifFormulaTrue(i1._1, i1._2, j1._2)
+    // var falseFormulas = formulas.filter{ case(k,v)=> v==false}.toList;
+    // println("falseFormulas size = "+ falseFormulas.length)
+    // var count = 1;
+    // while(count< 10000){
+    //   val formula = falseFormulas(rand.nextInt(falseFormulas.size)) 
+    //   count += 1
+    // }
   }
 }
